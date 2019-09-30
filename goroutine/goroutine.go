39fe974397be87028782fb984a8f7d0ab21eb3fc
yutomasamura <yuto.masumura@favy.co.jp>
2019-09-30 16:45:19 +0900

    goroutine_latter_half

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -62,6 +62,62 @@ func consumer(ch chan int, wg *sync.WaitGroup) {
 	fmt.Println("###################")
 }
 
+func producer2(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
+}
+
+// 引数でchannelの出力・入力を明示的に示す
+func multi2(first <-chan int, second chan<- int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
+	}
+}
+
+func multi4(second chan int, third chan int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 4
+	}
+}
+
+func goroutine4(ch chan string) {
+	for i := 0; i < 5; i++ {
+		ch <- "packet from 1"
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func goroutine5(ch chan string) {
+	for i := 0; i < 5; i++ {
+		ch <- "packet from 2"
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// Counter ...
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+// Inc ... 片方のgoroutineが書き込んでいる最中、他のgoroutineが干渉できないように
+func (c *Counter) Inc(key string) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+// Value ... 上記の処理の出力用
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
 func main() {
 	// goroutineの処理が完了するのを待つsync.WaitGroup
 	var wg sync.WaitGroup
@@ -120,4 +176,69 @@ func main() {
 	close(ch2)
 	time.Sleep(2 * time.Second)
 	fmt.Println("Done")
+
+	// 54. fan-out fan-in
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
+
+	go producer2(first)
+	go multi2(first, second)
+	go multi4(second, third)
+	for result := range third {
+		fmt.Println(result)
+	}
+
+	// 55. channelとselect
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go goroutine4(c1)
+	go goroutine5(c2)
+
+	//	複数のgoroutineが同時にmain()に対してchannelを渡す
+	for i := 0; i < 10; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
+	// 56. Default Selection と for break
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	// 任意の名前でbreak可能
+OuterLoop:
+	for {
+		select {
+		case <-tick:
+			fmt.Println(".tick")
+		case <-boom:
+			fmt.Println("BOOM!")
+			break OuterLoop
+		// ２つのchannel以外だと、以下の処理を走らせる
+		default:
+			fmt.Println(".")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+	fmt.Println("#####################")
+
+	// 57. sync.Mutex
+	// channelを使わずして、複数のgoroutineから値を取得するケース
+	c3 := Counter{v: make(map[string]int)}
+	go func() {
+		for i := 0; i < 10; i++ {
+			// c3["key"]++
+			c3.Inc("Key")
+		}
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			// c3["key"]++
+			c3.Inc("Key")
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	fmt.Println(c3, c3.Value("Key"))
 }
