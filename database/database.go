e339df24b022458b9d8ef7b928503eee62f9137d
mas <40512820+yuto0139@users.noreply.github.com>
2021-06-02 13:24:18 +0900

    Revert "some_comments_and_reviews_for_basic_part"

--- database/database.go
+++ database/database.go
@@ -19,27 +19,23 @@ type Person struct {
 
 func main() {
 	// 84. データベース操作
-	// データベースのコネクションをオープン
 	DbConnection, _ := sql.Open("sqlite3", "./example.sql")
 	defer DbConnection.Close()
 	cmd := `CREATE TABLE IF NOT EXISTS person(
               name STRING,
               age INT)`
-	// 作成したコマンドの実行
-	// データベースの情報を返す必要がないので、`_`へ代入
 	_, err := DbConnection.Exec(cmd)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	cmd = "INSERT INTO person (name, age) VALUES (?, ?)"
-	// 引数 "Mike", 24がコマンドの?へ代入
 	_, err = DbConnection.Exec(cmd, "Mike", 24)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	// '?'でsqlインジェクション防止 (不要な値はエスケープしてくれる)
+	// '?'でsqlインジェクション防止
 	cmd = "UPDATE person SET age = ? WHERE name = ?"
 	_, err = DbConnection.Exec(cmd, 30, "Mike")
 	if err != nil {
@@ -48,23 +44,20 @@ func main() {
 
 	// マルチセレクト
 	cmd = "SELECT * FROM person"
-	// レコードを取得するときはdb.Query(...)
 	rows, _ := DbConnection.Query(cmd)
-	// 処理が終わったらカーソルを閉じる
+	// 次のレコード取得のためにclose
 	defer rows.Close()
 	var pp []Person
 	for rows.Next() {
 		var p Person
 		// scanがstructの値にデータを挿入
 		err := rows.Scan(&p.Name, &p.Age)
-		// 一つずつエラーハンドリング
 		if err != nil {
 			log.Println(err)
 		}
 		// 一つ一つのインスタンスをpp(スライス)へ挿入
 		pp = append(pp, p)
 	}
-	// 一括でエラーハンドリングも可能
 	err = rows.Err()
 	if err != nil {
 		log.Fatalln(err)
@@ -75,7 +68,6 @@ func main() {
 
 	// シングルセレクト
 	cmd = "SELECT * FROM person where age = ?"
-	// １件取得するときはdb.QueryRow(...)
 	row := DbConnection.QueryRow(cmd, 20)
 	var p Person
 	err = row.Scan(&p.Name, &p.Age)
